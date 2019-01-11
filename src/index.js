c54f984a922a8c972a4dee2be65786a4f12b6c19
Konstantin Makarevich <krash1408@gmail.com>
2019-01-11 16:06:15 +0200

    add fn

--- src/index.js
+++ src/index.js
@@ -1,19 +1,36 @@
-const $ = x => document.querySelector(x);
-const $$ = x => document.querySelectorAll(x);
-
-const API = "https://randomuser.me/api/?results=30";
+const url = 'https://randomuser.me/api/?results=30&inc=gender,name,email,dob,phone,picture';
 let userPool = [];
-fetch(API)
-  .then(function ifError(response) {
-    if (response.ok) {
-      return response.json();
-    } else {
-      throw Error(response.statusText);
-    }
-  })
-  .then(function (data) {
-    userPool = data.results;
-  })
 
+function returnObject() {
+  fetch(url)
+    .then(function ifError(response) {
+      if (response.ok) {
+        return response.json();
+      }
+      else {
+        throw Error(response.statusText);
+      }
+    })
+    .then(function (data) {
+      userPool = data.results;
+    })
+    .catch(error => console.error(error));
+}
+returnObject();
 
-$('.card__photo').setAttribute('src',`url:${userPool[1].picture.large}`); 
+function friendsApp () {
+  
+}
+
+card = document.createElement('div')
+cardWrapper = document.createElement('div');
+cardPhoto = document.createElement('img');
+cardInfo = document.createElement('div');
+card.classList.add('card');
+cardWrapper.classList.add('card__wrapper');
+cardPhoto.classList.add('card__photo');
+cardInfo.classList.add('card__info');
+card.appendChild(cardWrapper);
+cardWrapper.appendChild(cardPhoto);
+cardWrapper.appendChild(cardInfo);
+// cardPhoto.setAttribute('src',`url('${userPool[0].picture.large}')`);
